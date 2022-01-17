25160df1d44bf6f84ce5851a67bbc8754b6c630a
yejun_1220@naver.com <yejun_1220@naver.com>
2022-01-17 22:32:15 +0900

    React-Basic(22.01.17)
    name.moudule.css

--- new version/react-basic-project/src/Button.js
+++ new version/react-basic-project/src/Button.js
@@ -1,15 +1,8 @@
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button({ text }) {
-  return (
-    <button
-      style={{
-        backgroundColor: "tomato",
-        color: "white",
-      }}>
-      {text}
-    </button>
-  );
+  return <button className={styles.btn}>{text}</button>;
 }
 
 Button.propTypes = {
