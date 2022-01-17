ab3c7eebd39ab3e680ed090cc9ffce6d56996f33
yejun_1220@naver.com <yejun_1220@naver.com>
2022-01-17 22:17:21 +0900

    React-Basic(22.01.17)
    npx install create-react-app

---
+++ new version/react-basic-project/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+      <Button text='Click' />
+    </div>
+  );
+}
+
+export default App;
