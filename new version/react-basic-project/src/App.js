c46af58531f8f4fbb3bb737765fdf82e6a5a3ec9
yejun_1220@naver.com <yejun_1220@naver.com>
2022-02-04 00:34:03 +0900

    React-Basic(22.02.04)
    Deps

--- new version/react-basic-project/src/App.js
+++ new version/react-basic-project/src/App.js
@@ -3,20 +3,38 @@ import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setValue] = useState(0);
+  const [count, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => {
-    setValue((prev) => prev + 1);
+    setCounter((prev) => prev + 1);
   };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
   console.log("Run all the time");
+
   useEffect(() => {
     console.log("CALL THE API..");
-  }, []);
+  }, []); // useEffect는 특정 상황에서만 실행되게 할 때 쓰인다. []안에는 state가 들어간다.
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]); // keyword가 변할 때만 useEffet가 실행된다.
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [count]);
 
   return (
     <div className='App'>
       <h1 className={styles.title}>Hello</h1>
       {/* 다른 클래스 이름을 사용하기 위해 기억하지 않아도 된다.(랜덤이름생성) */}
       <Button text='Click' />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here..'></input>
       <h1>{count}</h1>
       <button onClick={onClick}>click me!</button>
     </div>
