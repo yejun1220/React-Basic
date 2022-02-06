f4e0af0d296ea8bffb8210b3163085e2503703f6
yejun_1220@naver.com <yejun_1220@naver.com>
2022-02-06 21:00:50 +0900

    React-Basic(22.02.05)
    To Do List - 1

--- new version/react-basic-project/src/App.js
+++ new version/react-basic-project/src/App.js
@@ -1,18 +1,23 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
-  const onChange = (event) => setTodo(event.target.value);
+  const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
-    event.preventDefalut();
-    console.log(toDo);
+    event.preventDefault(); // 새로고침을 막아준다.
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+    console.log(toDos);
   };
 
   return (
     <div>
+      <h1>My To Dos({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
